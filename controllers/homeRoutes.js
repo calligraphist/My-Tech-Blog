05decb278eeac465464bd86bcb5768bbf91f342e
“anar <sehar.uzair23@gmail.com>
2023-05-11 10:16:45 -0300

    home routes added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
-
+const {User} = require("../model");
 
 router.get("/", (req,res)=>{
     res.render("homepage")
+});
+
+router.get("/", (req,res)=>{
+    res.render("login")
+});
+
+router.get("/", (req,res)=>{
+    res.render("dashboard")
+});
+
+router.get("/", (req,res)=>{
+    res.render("blog")
 })
+
 module.exports=router
