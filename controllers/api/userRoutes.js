ee1b65b0f47b29e6431db3aa28abafbfdaa4300e
“anar <sehar.uzair23@gmail.com>
2023-05-14 17:29:42 -0300

    express sessions applied

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,23 +2,23 @@ const router = require('express').Router();
 const { Blog, Comment, User } = require("../../models");
 //const withAuth = require('../../utils/auth');
 
-router.get("/", (req, res) => {
-  User.findAll({ include: [Blog, Comment] })
-    .then((userdata) => res.json(userdata))
-    .catch((err) => res.status(500).json(err));
-});
+// router.get("/", (req, res) => {
+//   User.findAll({ include: [Blog, Comment] })
+//     .then((userdata) => res.json(userdata))
+//     .catch((err) => res.status(500).json(err));
+// });
 
-router.get("/:id", (req, res) => {
-  User.findOne({ where: { id: req.params.id }, include: [Blog, Comment] })
-    .then((userdata) => res.json(userdata))
-    .catch((err) => res.json(err));
-});
+// router.get("/:id", (req, res) => {
+//   User.findOne({ where: { id: req.params.id }, include: [Blog, Comment] })
+//     .then((userdata) => res.json(userdata))
+//     .catch((err) => res.json(err));
+// });
 
-router.post("/", (req, res) => {
-  User.create(req.body)
-    .then((userdata) => res.json(userdata))
-    .catch((err) => res.json(err));
-});
+// router.post("/", (req, res) => {
+//   User.create(req.body)
+//     .then((userdata) => res.json(userdata))
+//     .catch((err) => res.json(err));
+// });
 
 router.put("/:id", (req, res) => {
   User.update(req.body, { where: { blog_id: req.params.id } })
@@ -36,6 +36,62 @@ router.delete("/:id", (req, res) => {
 // router.get("/", async (req, res) => {
 //   res.render("all", { user });
 // });
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
 
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports=router
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,7 +13,8 @@ router.get("/login", (req,res)=>{
 
 // Send the rendered dashboard Handlebars.js template back as the response
 router.get("/dashboard", (req,res)=>{
-    res.render("dashboard")
+    const name ="test";
+    res.render("dashboard", { name: name }, {blog})
 });
 
 // Send the rendered blog Handlebars.js template back as the response
