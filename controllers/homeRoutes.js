119dcafe6289488c1ea40f38d8dd81381a200dd1
“anar <sehar.uzair23@gmail.com>
2023-05-14 15:50:31 -0300

    got login button work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,22 +2,22 @@ const router = require('express').Router();
 //const {User} = require("../model");
 
 // Send the rendered homepage Handlebars.js template back as the response
-router.get("/", (req,res)=>{
+router.get("/homepage", (req,res)=>{
     res.render("homepage")
 });
 
 // Send the rendered login Handlebars.js template back as the response
-router.get("/", (req,res)=>{
+router.get("/login", (req,res)=>{
     res.render("login")
 });
 
 // Send the rendered dashboard Handlebars.js template back as the response
-router.get("/", (req,res)=>{
+router.get("/dashboard", (req,res)=>{
     res.render("dashboard")
 });
 
 // Send the rendered blog Handlebars.js template back as the response
-router.get("/", (req,res)=>{
+router.get("/blog", (req,res)=>{
     res.render("blog")
 })
 
