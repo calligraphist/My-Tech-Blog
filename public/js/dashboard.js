6ac9709986776b695d30f72bc0dc5c5f218d54bf
“anar <sehar.uzair23@gmail.com>
2023-05-14 21:03:17 -0300

    /blog replaced by /dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -14,7 +14,7 @@ const newFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/blog');
+        document.location.replace('/dashboard');
       } else {
         alert('Failed to create blog');
       }
@@ -25,12 +25,12 @@ const newFormHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/blog/${id}`, {
+      const response = await fetch(`/api/blog/${id}`, {
         method: 'DELETE',
       });
   
       if (response.ok) {
-        document.location.replace('/blog');
+        document.location.replace('/dashboard');
       } else {
         alert('Failed to delete blog');
       }
@@ -42,6 +42,6 @@ const newFormHandler = async (event) => {
     .addEventListener('submit', newFormHandler);
   
   document
-    .querySelector('#blog-list')
+    .querySelector('.blog-list')
     .addEventListener('click', delButtonHandler);
   
